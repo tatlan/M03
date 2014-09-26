e614384f88ee578a09dd8cae2f0bf9f8cc13c707
JuanAlfonso <JuanAlfonso@Lenovo>
2014-09-26 16:36:19 +0200

    Es el primero, no hay modificaciones

---
+++ src/m03/M03.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m03;
+
+/**
+ *
+ * @author JuanAlfonso
+ */
+public class M03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1=4;
+        int n2=5;
+        int resultado;
+        resultado=n1+n2;
+        System.out.println("El resultado es: "+resultado);
+        
+       
+        
+        
+        
+                
+    }
+    
+}
